ed3da8eaacf793b63be5430d57ca154799bac654
femmytedrey <femmytedrey@gmail.com>
2023-12-15 21:54:57 +0100

    filter button rendered dynamically

--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Categories = ({filterItems}) => {
+const Categories = ({filterItems, categories}) => {
   return <div className = 'btn-container'>
-    <button className = 'filter-btn' onClick = {() => filterItems('all')} >all</button>
+    {/* dynamic rendering of the button */}
+    {categories.map((category, index) => {
+      return(
+        <button type = 'button' className= 'filter-btn' key={index} onClick = {()=> filterItems(category)} > {category} </button>
+      )
+    })}
+    
+
+
+
+    {/* manual rendering of the buttons */}
+    {/* <button className = 'filter-btn' onClick = {() => filterItems('all')} >all</button>
     <button className = 'filter-btn' onClick = {() => filterItems('breakfast')} >breakfast</button>
+    <button className = 'filter-btn' onClick = {() => filterItems('lunch')} >lunch</button>
+    <button className = 'filter-btn' onClick = {() => filterItems('shakes')} >shakes</button> */}
   </div>;
 };
 
